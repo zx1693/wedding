bd5f86a8f28b11603ea74687458d2a2827db3ac6
me <1581575616@qq.com>
2019-02-27 09:59:09 +0800

    three

--- js/vipcrea.js
+++ js/vipcrea.js
@@ -1,3 +1,8 @@
 $("#vipcrea").click(function () {
     $(".vipTable").hide().next().show()
 })
+
+$(".ending-line").click(function () {
+    // $(".vipTable").hide().next().show();
+    $(".vip-detail").hide().prev().show();
+})
